13f3c7b50d560ce49fdc98a0b0b845a640e9899d
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2025-10-22 13:57:20 +0200

    [^/] Code cleanup and rearrangements.

--- src/me_HardwareClockScaling.cpp
+++ src/me_HardwareClockScaling.cpp
@@ -2,19 +2,14 @@
 
 /*
   Author: Martin Eden
-  Last mod.: 2025-10-19
+  Last mod.: 2025-10-21
 */
 
 /*
   This implementation is doing closest match calculations
 
   Imagine we can do only 10 s delays and asked for 27 s delay.
-
-  We can't do it exactly, so our options are:
-
-    * Fail because we can't do it exactly
-    * Do 20 s delay, we never overshoot
-    * Do 30 s delay because it's closest match
+  We'll do 30 s delay because it's closest match.
 */
 
 #include <me_HardwareClockScaling.h>
@@ -25,26 +20,46 @@ using namespace me_HardwareClockScaling;
 
 const TUint_4 BaseFreq = F_CPU;
 
-// [Internal] Check hardware spec
-static TBool CheckSpec(
-  TClockScalingOptions * ScaleOpts
+// [Internal] Check scale
+static TBool CheckScale(
+  TClockScale Scale
 )
 {
+  return (Scale.Prescale_PowOfTwo <= 16);
+}
+
+// [Internal] Check hardware setting
+static TBool CheckSetting(
+  TClockScaleSetting Setting
+)
+{
+  return
+    (Setting.CounterNumBits <= 16) &&
+    (Setting.Prescale_PowOfTwo <= 16);
+}
+
+// [Internal] Check hardware spec
+static TBool CheckSpec(
+  TClockScalingOptions ScaleOpts
+)
+{
+  TClockScaleSetting Setting;
   TUint_1 Index;
 
-  if (ScaleOpts->NumPrescalerValues == 0)
+  if (ScaleOpts.NumPrescalerValues == 0)
     return false;
 
-  for (Index = 0; Index < ScaleOpts->NumPrescalerValues; ++Index)
-  {
-    if (ScaleOpts->CounterNumBits > 16)
-      return false;
+  Setting.CounterNumBits = ScaleOpts.CounterNumBits;
 
-    if (ScaleOpts->Prescales_PowOfTwo[Index] > 16)
+  for (Index = 0; Index < ScaleOpts.NumPrescalerValues; ++Index)
+  {
+    Setting.Prescale_PowOfTwo = ScaleOpts.Prescales_PowOfTwo[Index];
+
+    if (!CheckSetting(Setting))
       return false;
 
     if (Index > 0)
-      if (ScaleOpts->Prescales_PowOfTwo[Index] <= ScaleOpts->Prescales_PowOfTwo[Index - 1])
+      if (ScaleOpts.Prescales_PowOfTwo[Index] <= ScaleOpts.Prescales_PowOfTwo[Index - 1])
         return false;
   }
 
@@ -74,72 +89,127 @@ static TBool GetNumUnitsForLength(
 }
 
 /*
-  Represent frequency as hardware duration
+  [Interface] Represent frequency as hardware duration
 */
-TBool me_HardwareClockScaling::CalculateClockScale(
+TBool me_HardwareClockScaling::CalculateClockScale_Spec(
+  TClockScale * Scale,
+  TUint_4 Freq_Hz,
+  TClockScaleSetting Setting
+)
+{
+  if (!CheckSetting(Setting))
+    return false;
+
+  return Freetown::CalculateClockScale_Spec(Scale, Freq_Hz, Setting);
+}
+
+/*
+  [Interface] Represent frequency as hardware duration using list of options
+*/
+TBool me_HardwareClockScaling::CalculateClockScale_Specs(
   TClockScale * Scale,
   TUint_4 Freq_Hz,
   TClockScalingOptions ScaleOpts
 )
 {
-  TUint_4 CounterMaxValue;
-  TUint_1 Index;
-  TUint_4 ScaledFreq;
-  TUint_4 CounterLimit;
-
-  if (!CheckSpec(&ScaleOpts))
+  if (!CheckSpec(ScaleOpts))
     return false;
 
-  CounterMaxValue = (1L << ScaleOpts.CounterNumBits);
-
-  for (Index = 0; Index < ScaleOpts.NumPrescalerValues; ++Index)
-  {
-    ScaledFreq = BaseFreq >> ScaleOpts.Prescales_PowOfTwo[Index];
-
-    if (!GetNumUnitsForLength(&CounterLimit, ScaledFreq, Freq_Hz))
-      continue;
-
-    if (CounterLimit == 0)
-      continue;
-
-    if (CounterLimit > CounterMaxValue)
-      continue;
-
-    Scale->Prescale_PowOfTwo = ScaleOpts.Prescales_PowOfTwo[Index];
-    Scale->CounterLimit = CounterLimit - 1;
-
-    return true;
-  }
-
-  return false;
-}
-
-// [Internal] Check scale
-static TBool CheckScale(
-  TClockScale Scale
-)
-{
-  if (Scale.Prescale_PowOfTwo > 16)
-    return false;
-
-  return true;
+  return Freetown::CalculateClockScale_Specs(Scale, Freq_Hz, ScaleOpts);
 }
 
 /*
-  Represent hardware duration as frequency
+  [Interface] Represent hardware duration as frequency
 */
 TBool me_HardwareClockScaling::CalculateFrequency(
   TUint_4 * Freq_Hz,
   TClockScale Scale
 )
 {
-  TUint_4 ScaledFreq;
-  TUint_4 CounterLimit;
-
   if (!CheckScale(Scale))
     return false;
 
-  ScaledFreq = BaseFreq >> Scale.Prescale_PowOfTwo;
+  return Freetown::CalculateFrequency(Freq_Hz, Scale);
+}
+
+/*
+  Represent frequency as hardware duration
+*/
+TBool Freetown::CalculateClockScale_Spec(
+  TClockScale * Scale,
+  TUint_4 Freq_Hz,
+  TClockScaleSetting Setting
+)
+{
+  TUint_4 CounterMaxValue;
+  TUint_4 ClockSlowdown;
+  TUint_4 ScaledFreq;
+  TUint_4 CounterLimit;
+
+  CounterMaxValue = (1L << Setting.CounterNumBits);
+
+  ClockSlowdown = (1L << Setting.Prescale_PowOfTwo);
+
+  if (!GetNumUnitsForLength(&ScaledFreq, BaseFreq, ClockSlowdown))
+    return false;
+
+  if (!GetNumUnitsForLength(&CounterLimit, ScaledFreq, Freq_Hz))
+    return false;
+
+  if (CounterLimit == 0)
+    return false;
+
+  if (CounterLimit > CounterMaxValue)
+    return false;
+
+  Scale->Prescale_PowOfTwo = Setting.Prescale_PowOfTwo;
+  Scale->CounterLimit = CounterLimit - 1;
+
+  return true;
+}
+
+/*
+  Represent frequency as hardware duration using list of options
+*/
+TBool Freetown::CalculateClockScale_Specs(
+  TClockScale * Scale,
+  TUint_4 Freq_Hz,
+  TClockScalingOptions ScaleOpts
+)
+{
+  TClockScaleSetting Setting;
+  TUint_1 Index;
+
+  Setting.CounterNumBits = ScaleOpts.CounterNumBits;
+
+  for (Index = 0; Index < ScaleOpts.NumPrescalerValues; ++Index)
+  {
+    Setting.Prescale_PowOfTwo = ScaleOpts.Prescales_PowOfTwo[Index];
+
+    if (Freetown::CalculateClockScale_Spec(Scale, Freq_Hz, Setting))
+      return true;
+  }
+
+  return false;
+}
+
+/*
+  Represent hardware duration as frequency
+*/
+TBool Freetown::CalculateFrequency(
+  TUint_4 * Freq_Hz,
+  TClockScale Scale
+)
+{
+  TUint_4 ScaledFreq;
+  TUint_4 ClockSlowdown;
+  TUint_4 CounterLimit;
+
+  ClockSlowdown = (1L << Scale.Prescale_PowOfTwo);
+
+  if (!GetNumUnitsForLength(&ScaledFreq, BaseFreq, ClockSlowdown))
+    return false;
+
   CounterLimit = (TUint_4) Scale.CounterLimit + 1;
 
   if (!GetNumUnitsForLength(Freq_Hz, ScaledFreq, CounterLimit))
@@ -152,4 +222,5 @@ TBool me_HardwareClockScaling::CalculateFrequency(
   2025-10-15
   2025-10-16
   2025-10-19
+  2025-10-21
 */
--- src/me_HardwareClockScaling.h
+++ src/me_HardwareClockScaling.h
@@ -2,31 +2,7 @@
 
 /*
   Author: Martin Eden
-  Last mod.: 2025-10-19
-*/
-
-/*
-  In ATmega328 hardware counters and UART (and maybe other modules)
-  need to set speed slower than base clock speed.
-
-  They are using "prescaler". Which is just integer power of two.
-  For prescaler 1024 hardware module will run at each 1024th tick.
-
-  (Well, that's speculation and abstraction for explanation.)
-
-  Prescaler defines temporal granularity.
-
-  And to store number of time granules another integer is used.
-  For UART it's 10-bit int, for counters it's 8- or 16-bit.
-*/
-
-/*
-  We do not like duplicated logic and we want back conversion:
-  from duration to frequency. So we wrote this module.
-*/
-
-/*
-  C language is wrong tool for this but we have no options now.
+  Last mod.: 2025-10-22
 */
 
 #pragma once
@@ -36,7 +12,7 @@
 namespace me_HardwareClockScaling
 {
   /*
-    Clock slowdown factor and number of clock cycles
+    Clock slowdown: slowdown factor and counter value
 
     For number of cycles: 0 means 1, 1 means 2 etc.
   */
@@ -46,12 +22,22 @@ namespace me_HardwareClockScaling
     TUint_2 CounterLimit;
   };
 
+  /*
+    Clock slowdown limits: slowdown factor and maximum counter value
+  */
+  struct TClockScaleSetting
+  {
+    TUint_1 Prescale_PowOfTwo;
+    TUint_1 CounterNumBits;
+  };
+
   const TUint_1 MaxPrescalerValues = 7;
 
   /*
-    Available prescalers and counter size
+    List of clock slowdown limits: slowdown factors and maximum counter
+    value
 
-    Prescalers array should store ascending values.
+    Slowdown factors should come in ascending order.
   */
   struct TClockScalingOptions
   {
@@ -60,8 +46,17 @@ namespace me_HardwareClockScaling
     TUint_1 CounterNumBits;
   };
 
-  // Convert frequency to clock scale
-  TBool CalculateClockScale(
+  // ( Interface functions
+
+  // Convert frequency to clock scale, given limit
+  TBool CalculateClockScale_Spec(
+    TClockScale * ClockScale,
+    TUint_4 Freq_Hz,
+    TClockScaleSetting ScaleSetting
+  );
+
+  // Convert frequency to clock scale, given limits
+  TBool CalculateClockScale_Specs(
     TClockScale * ClockScale,
     TUint_4 Freq_Hz,
     TClockScalingOptions ScalingOpts
@@ -72,6 +67,16 @@ namespace me_HardwareClockScaling
     TUint_4 * Freq_Hz,
     TClockScale ClockScale
   );
+
+  // )
+
+  // Real implementation without input checks
+  namespace Freetown
+  {
+    TBool CalculateClockScale_Spec(TClockScale *, TUint_4, TClockScaleSetting);
+    TBool CalculateClockScale_Specs(TClockScale *, TUint_4, TClockScalingOptions);
+    TBool CalculateFrequency(TUint_4 *, TClockScale);
+  }
 
   namespace AtMega328
   {
@@ -86,4 +91,5 @@ namespace me_HardwareClockScaling
   2025-10-15
   2025-10-16
   2025-10-18
+  2025-10-21
 */
